4a4257c4f1817e6ad7e8641d4b9d3765a4fc476e
Jonathan Novaretti <jonn.novaretti@gmail.com>
2021-07-17 16:13:02 -0300

    Refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,50 @@
 class ArticlesController < ApplicationController
-  def show
-    @article = Article.find(params[:id])
-  end
+  before_action :set_article, only: %i[show edit update destroy]
+
+  def show; end
 
   def index
     @articles = Article.all
   end
 
   def new
-    
+    @article = Article.new
   end
 
-  def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
+  def edit; end
 
-    redirect_to @article
+  def create
+    @article = Article.new(read_article_from_params)
+
+    if @article.save
+      flash[:notification] = 'Article was created successfully'
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @article.update(read_article_from_params)
+      flash[:notice] = 'Article was updated successfully'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def read_article_from_params
+    params.require(:article).permit(:title, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index, :create, :new]
+  resources :articles
 end
