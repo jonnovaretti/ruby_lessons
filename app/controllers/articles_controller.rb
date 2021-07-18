208254af50819cdf9a3d0dc9da140aaf94114899
Jonathan Novaretti <jonn.novaretti@gmail.com>
2021-07-18 16:28:12 -0300

    Including pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   def show; end
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 6)
   end
 
   def new
