667fcea617a9c0584f3abc1417b84fc17fa33938
jonnovaretti <jonn.novaretti@gmail.com>
2021-07-20 21:00:51 -0300

    Adding creating category test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,26 @@
 class CategoriesController < ApplicationController
-    
-    def new
-        
-    end
+  def new
+    @category = Category.new
+  end
 
-    def index
-        
-    end
+  def index; end
 
-    def show
-        
+  def show; end
+
+  def create
+    category = Category.new(category_name_from_params)
+
+    if category.save
+      flash[:notice] = 'Category has been saved'
+      redirect_to category
+    else
+      render 'new'
     end
+  end
+
+  private
+
+  def category_name_from_params
+    params.require(:category).permit(:name)
+  end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,30 +1,29 @@
-require "test_helper"
+require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-
   setup do
-    @category = Category.create(name: "Life")
+    @category = Category.create(name: 'Life')
   end
 
-  test "should get index" do
+  test 'should get index' do
     get categories_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_category_url
     assert_response :success
   end
 
-  # test "should create category" do
-  #   assert_difference('Category.count') do
-  #     post categories_url, params: { category: {  } }
-  #   end
+  test 'should create category' do
+    assert_difference('Category.count', 1) do
+      post categories_url, params: { category: { name: 'Travel'  } }
+    end
 
-  #   assert_redirected_to category_url(Category.last)
-  # end
+    assert_redirected_to category_url(Category.last)
+  end
 
-  test "should show category" do
+  test 'should show category' do
     get category_url(@category)
     assert_response :success
   end
